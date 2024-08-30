4a217a8a2215e36e7bde7b4ae9c20118618cfae9
Lucas Andrusievicz <lucasandru@gmail.com>
2024-08-30 18:57:31 -0300

    Add files via upload

---
+++ Atv18Exc4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+
+{
+    printf("[ x ] geometria e balanceamento\n");
+    printf("[ x ] troca de oleo\n");
+    printf("[ x ] filtro de combustivel\n");
+    printf("[ x ] troca bico injetor\n");
+
+    return 0;
+
+}
