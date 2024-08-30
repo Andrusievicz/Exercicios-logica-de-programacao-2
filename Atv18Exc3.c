224a710eb071af703639a84db77f9613ab3aea13
Lucas Andrusievicz <lucasandru@gmail.com>
2024-08-30 18:57:23 -0300

    Add files via upload

---
+++ Atv18Exc3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+
+{
+
+printf("Ingredientes: 2 xicaras de açucar, 4 colheres de margarina, 1 e 1/2 xicara de leite, 3 xicaras de farinha, 3 ovos, 1 colher de fermento\n");
+printf("Bata as claras em neve e reserve.\n");
+printf("Misture as gemas, a margarina e o acucar ate obter uma massa homogenea.\n");
+printf("Acrescente o leite e a farinha de trigo aos poucos, sem parar de bater.\n");
+printf("Por ultimo, adicione as claras em neve e o fermento.\n");
+printf("Despeje a massa em uma forma grande de furo central untada e enfarinhada.\n");
+printf("Asse em forno medio 180 C, preaquecido, por aproximadamente 40 minutos ou ao furar o bolo com um garfo, este saia limpo.\n");
+return 0;
+}
+
