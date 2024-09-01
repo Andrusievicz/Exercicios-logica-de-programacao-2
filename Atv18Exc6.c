72a03d361e3068de954717260ddd7e25afcb3cee
Lucas Andrusievicz <lucasandru@gmail.com>
2024-08-31 21:47:59 -0300

    Add files via upload

---
+++ Atv18Exc6.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main ()
+
+{
+    printf("1 - JavaScript\n");
+    printf("2 - SQL\n");
+    printf("3 - Java\n");
+    printf("4 - Bash/Shell\n");
+    printf("5 - Python\n");
+    printf("6 - C#\n");
+    printf("7 - PHP\n");
+    printf("8 - C++\n");
+    printf("9 - C\n");
+    printf("10 - TypeScript\n");
+
+    return 0;
+}
