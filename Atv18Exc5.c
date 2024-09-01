636498a771494da3ad147bb9e461eb5ec4926851
Lucas Andrusievicz <lucasandru@gmail.com>
2024-08-31 21:47:51 -0300

    Add files via upload

---
+++ Atv18Exc5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main ()
+
+{
+    printf("Nome completo: Lucas Andrusievicz\n");
+    printf("Endereco: Av santa bernadethe\n");
+    printf("Numero: 841\n");
+    printf("Completemento: Perto a igreja menino jesus de praga\n");
+    printf("CEP: 81010200\n");
+    printf("Bairro: Lindoia\n");
+    printf("Estado: Parana\n");
+    printf("Pais: Brasil\n");
+
+}
