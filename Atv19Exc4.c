7a676b64fd21daa59c728dd6e1adef69503df245
Lucas Andrusievicz <lucasandru@gmail.com>
2024-09-02 18:51:52 -0300

    Add files via upload

---
+++ Atv19Exc4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main ()
+
+{
+    int valor1;
+
+    printf("Digite um valor ");
+    scanf("%i", &valor1);
+    printf("\nO valor digitado foi: %i\n", valor1);
+
+    system ("pause");
+
+    return 0;
+}
+
