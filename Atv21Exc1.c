c3e04ed86394d5c8a1a811aac2d07fd78b0d7154
Lucas Andrusievicz <lucasandru@gmail.com>
2024-09-07 19:01:22 -0300

    Add files via upload

---
+++ Atv21Exc1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main ()
+
+{
+
+    printf("DOMINGO");
+    printf("\nSEGUNDA");
+    printf("\nTERCA");
+    printf("\nQUARTA");
+    printf("\nQUINTA");
+    printf("\nSEXTA");
+    printf("\nSABADO\n\n");
+
+    system ("pause");
+
+    return 0;
+
+}
